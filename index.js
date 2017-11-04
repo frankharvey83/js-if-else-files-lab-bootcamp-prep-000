3a143008ef2516c6e1bd4ad012ea5394940ec603
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-04 23:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var height = "74"
 module.exports = { name, height
 }
 
-if (name === "susan") {
+if (name === "Susan") {
   console.log("That name is correct")
 } else {
   console.log("Expected the name to be Susan")
