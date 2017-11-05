cfaab3f8f0d104106fa1f165fb3633b26a1fa37b
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-05 00:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 
@@ -10,4 +10,10 @@ if (name === "Susan") {
   console.log("That name is correct")
 } else {
   console.log("Expected the name to be Susan")
+}
+
+if (height === "74") {
+  console.log("That height is correct")
+} else {
+  console.log("Expected the height to be 70")
 }
